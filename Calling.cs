c09beb4b6be972998eb51aa1aa48b1576319ce07
KanikaSaini01 <105622526+KanikaSaini01@users.noreply.github.com>
2022-05-22 22:31:31 +0530

    Add files via upload

---
+++ Calling.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSConsoleDemo
+{
+    public abstract class Calling //we cannot create object
+    {
+        public void MyCalls()
+        {
+            Console.WriteLine("My calls");
+        }
+        public abstract void Dial(); //only declaration no definition
+    }
+    public class MobileConnection : Calling
+    {
+        public override void Dial()
+        {
+            Console.WriteLine("Mobile call");
+        }
+    }
+}
